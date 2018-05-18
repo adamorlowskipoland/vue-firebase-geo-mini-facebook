cf7be9791d4c2d80cedcc96f6bbb667bda9184c6
Adam <adamorlowskipoland@icloud.com>
2018-05-18 02:18:58 +0200

    add comments to the wall

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -3,22 +3,77 @@
     <div v-if="profile"
          class="card">
       <h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
+      <ul class="comments collection">
+        <li>Comment</li>
+      </ul>
+      <form @submit.prevent="addComment">
+        <div class="field">
+          <label for="comment">Add a comment</label>
+          <input type="text"
+                 name="comment"
+                 id="comment"
+                 v-model="newComment">
+          <p v-if="feedback"
+             class="red-text center">{{ feedback }}</p>
+        </div>
+        <button class="btn deep-purple">Add</button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
   import db from '@/firebase/init';
+  import firebase from 'firebase';
 
   export default {
     name: 'ViewProfile',
     data() {
       return {
         profile: null,
+        newComment: null,
+        feedback: null,
+        user: null,
       };
+    },
+    methods: {
+      addComment() {
+        if (this.newComment) {
+          this.feedback = null;
+          db.collection('comments')
+            .add({
+              to: this.$route.params.id,
+              from: this.user.id,
+              content: this.newComment,
+              time: Date.now(),
+            })
+            .then(() => {
+              this.newComment = null;
+            })
+            .catch((err) => {
+              throw new Error(err);
+            });
+          this.newComment = null;
+        } else {
+          this.feedback = 'Enter a comment';
+        }
+      },
     },
     created() {
       const ref = db.collection('users');
+
+      ref.where('user_id', '==', firebase.auth().currentUser.uid)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((doc) => {
+            this.user = doc.data();
+            this.user.id = doc.id;
+          });
+        })
+        .catch((err) => {
+          throw new Error(err);
+        });
+
       ref.doc(this.$route.params.id)
         .get()
         .then((user) => {
